33d22882a65816f6fbfed91ecc55bc36d52789da
Bandaru srinivas <bandarusrinivas626@gmail.com>
2026-06-10 10:31:31 +0530

    Worked on optional class methodes

--- Java8/optional.java
+++ Java8/optional.java
@@ -3,11 +3,20 @@ import java.util.Optional;
 public class optional {
 
     public static void main(String[] args) {
-        Optional <String> s1=Optional.ofNullable(null);
+        // Optional <String> s1=Optional.ofNullable(null);
+     
 
         Optional<String> s=Optional.empty();
 
-        String name=s.orElse("Default");
+        String name=s.orElse("Srinivas");
         System.out.println(name);
     }
 }
+
+// of will not allow null values  ...
+// of nullable it will allow null values if vlaue is null returns Optional.empty  ...
+//empty returns empty optional ...
+//orElse we can set default value  ...
+// empty returns a optional values
+//is empty returns true if value is exsists
+//is present returns true if value is exsists
